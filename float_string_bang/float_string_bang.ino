274b0a03efa841a1a54aacae8ecd020b1ecfa224
Alexandros Drymonitis <alexdrymonitis@gmail.com>
2015-12-24 19:48:00 +0200

    Update float_string_bang.ino

--- float_string_bang/float_string_bang.ino
+++ float_string_bang/float_string_bang.ino
@@ -1,3 +1,14 @@
+#define MAX_RAND 100
+
+String strings[3] = { "one_string", "another_string", "last_string" };
+int string_counter = 0;
+
+String list_strings[3] = { "a_string_in_a_list", "another_list_string", "last_list_string" };
+int list_str_cnt = 0;
+
+float a_float, another_float, negative_float;
+int an_int;
+
 void setup() {
   Serial.begin(9600);
 }
@@ -5,17 +16,45 @@ void setup() {
 void loop() {
   if(Serial.available()){
     byte in_byte = Serial.read();
-    if(in_byte == 0){
-      Serial.print("receive_float\t");
-      Serial.println(PI);
-    }
-    else if(in_byte == 1){
-      Serial.print("receive_string\t");
-      Serial.println("this_is_a_string");
-    }
-    else if(in_byte == 2){
-      Serial.print("receive_bang\t");
-      Serial.println("bang");
+    
+    switch(in_byte){
+      case 0:
+        a_float = (float)random(MAX_RAND) / (float)MAX_RAND;
+        Serial.print("a_float\t");
+        Serial.println(a_float);
+        break;
+
+      case 1:
+        Serial.print("a_string\t");
+        Serial.println(strings[string_counter]);
+        string_counter++;
+        if(string_counter >= 3) string_counter = 0;
+        break;
+
+      case 2:
+        Serial.print("a_bang\t");
+        Serial.println("bang");
+        break;
+
+      case 3:
+        negative_float = ((float)random(MAX_RAND) / (float)MAX_RAND) * -1.0;
+        Serial.print("negative_float\t");
+        Serial.println(negative_float);
+        break;
+
+      case 4:
+        an_int = random(MAX_RAND);
+        another_float = (float)random(MAX_RAND) / (float)MAX_RAND;
+        Serial.print("mixed_list\t");
+        Serial.print(list_strings[list_str_cnt]); Serial.print("\t");
+        Serial.print(another_float); Serial.print("\t");
+        Serial.print(an_int); Serial.print("\t");
+        // the bang can't be included in a list in Pd, so it will be output last
+        // no matter where it's placed in the Arduino code
+        Serial.println("bang");
+        list_str_cnt++;
+        if(list_str_cnt >= 3) list_str_cnt = 0;
+        break;
     }
   }
 }
