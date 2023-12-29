828e51a2e76b45fcdb347179cc5f473673a6f824
Alexandros Drymonitis <alexdrymonitis@gmail.com>
2023-12-29 12:34:31 +0200

    Update serial_write.ino

--- serial_write/serial_write.ino
+++ serial_write/serial_write.ino
@@ -15,40 +15,40 @@
  */
 
 // analog values array size, must be constant
-const int num_of_analog_pins = 3;
+const int numAnalogPins = 3;
 // digital values array size, must be constant
-const int num_of_digital_pins = 3;
+const int numDigitalPins = 3;
 
 // assemble number of bytes we need
 // analog values are being split in two, so their number times 2
 // and we need a unique byte to denote the beginning of the data stream
-const int num_of_bytes = (num_of_analog_pins * 2) + num_of_digital_pins + 1;
+const int numBytes = (numAnalogPins * 2) + numDigitalPins + 1;
 
 // array to store all bytes
-byte transfer_array[num_of_bytes];
+byte transferArray[numBytes];
 
 void setup() {
-  for(int i = 0; i < num_of_digital_pins; i++) pinMode((i + 2), INPUT_PULLUP);
+  for(int i = 0; i < numDigitalPins; i++) pinMode((i + 2), INPUT_PULLUP);
   
   Serial.begin(57600);
 }
 
 void loop() {
-  transfer_array[0] = 0xc0; // denote beginning of data
+  byte transferArray[0] = 0xc0; // denote beginning of data
   int index = 1; // index offset
   
   // store the analog values to the array
-  for(int i = 0; i < num_of_analog_pins; i++){
-    int analog_val = analogRead(i);
+  for(int i = 0; i < numAnalogPins; i++){
+    int analogVal = analogRead(i);
     // split analog values so they can retain their 10-bit resolution
-    transfer_array[index++] = analog_val & 0x007f;
-    transfer_array[index++] = analog_val >> 7;
+    byte transferArray[index++] = analogVal & 0x007f;
+    byte transferArray[index++] = analogVal >> 7;
   }
 
   // store the digital values to the array
-  for(int i = 0; i < num_of_digital_pins; i++)
-    transfer_array[index++] = !digitalRead(i + 2);
+  for(int i = 0; i < numDigitalPins; i++)
+    byte transferArray[index++] = !digitalRead(i + 2);
 
   // transfer bytes over serial
-  Serial.write(transfer_array, num_of_bytes);
+  Serial.write(byte transferArray, numBytes);
 }
